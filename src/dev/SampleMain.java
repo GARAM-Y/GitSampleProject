849f02c7ebdf33aec0db200c9a74e6f57b90a5c1
ygr <yeokawa@gmail.com>
2019-11-18 10:29:25 +0900

    epic-05 hello world 일문 추가, test 문자열 삭제

--- src/dev/SampleMain.java
+++ src/dev/SampleMain.java
@@ -4,10 +4,9 @@ public class SampleMain {
     static void main(String[] args){
 
         String str = "안녕 세상";
-        String enStr = "Hello World!!!";
+        String jpStr = "ハローワールド";
 
         System.out.println(str);
-        System.out.println(enStr);
-        System.out.println("test");
+        System.out.println(jpStr);
     }
 }
