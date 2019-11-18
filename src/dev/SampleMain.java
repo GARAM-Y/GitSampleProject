02e97f8edcdd403d3de904f446ece3fdfe96cd86
ygr <yeokawa@gmail.com>
2019-11-18 09:33:37 +0900

    epic-01, epic-02 merge

--- src/dev/SampleMain.java
+++ src/dev/SampleMain.java
@@ -4,6 +4,9 @@ public class SampleMain {
     static void main(String[] args){
 
         String str = "안녕 세상";
+        String enStr = "Hello World!!!";
+
         System.out.println(str);
+        System.out.println(enStr);
     }
 }
